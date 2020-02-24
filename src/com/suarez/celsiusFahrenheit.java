15bf6951aa1a4162e93925bc215baeaf2cd62a62
gandev20 <Varsha90!>
2020-02-24 11:20:57 -0600

    Updated

--- src/com/suarez/celsiusFahrenheit.java
+++ src/com/suarez/celsiusFahrenheit.java
@@ -7,12 +7,12 @@ public class celsiusFahrenheit {
 This program will convert Fahrenheit to Celsius for certain data
  */
     public static void main (String[] args) {
-        Scanner console = new Scanner(System.in);
+        Scanner console = new Scanner(System.in); //initialize scanner
 
         System.out.print("What's the temperature outside (in Fahrenheit)?");
-        int fahTemp = console.nextInt();
+        int fahTemp = console.nextInt(); //next int is temperature in F
 
-        double celsiusTemp = (fahTemp-32)/1.8;
+        double celsiusTemp = (fahTemp-32)/1.8; //conversion from F to C
         System.out.printf("It is %.2f", celsiusTemp);
         System.out.print(" degrees Celsius outside! Have a nice day!");
         }
--- src/com/suarez/electoralVotes.java
+++ src/com/suarez/electoralVotes.java
@@ -5,27 +5,27 @@ import java.util.*;
 
 public class electoralVotes {
     public static void main(String args[]) throws FileNotFoundException{
-        Scanner input = new Scanner(new File("C:\\Users\\vrung\\IdeaProjects\\Beginning_Java\\src\\com\\suarez\\poll.txt"));
-        int obama = 0;
-        int mccain = 0;
+        Scanner input = new Scanner(new File("C:\\Users\\vrung\\IdeaProjects\\Beginning_Java\\src\\com\\suarez\\poll.txt")); //this scanner will read the file
+        int obama = 0; //initialize sum of obama votes
+        int mccain = 0; //initialize sum of mccain votes
         while(input.hasNextLine()) {
             String line = input.nextLine();
-            Scanner elect = new Scanner(line);
-            int count = 1;
+            Scanner elect = new Scanner(line); //this scanner will read each line
+            int count = 1; //counter to determine where to allocate each vote
             while(elect.hasNext()) {
                 while (elect.hasNextInt()) {
                     if (count == 1) {
-                        obama = obama + elect.nextInt();
+                        obama = obama + elect.nextInt(); //add to obama sum
                     }
                     else if (count == 2) {
-                        mccain = mccain + elect.nextInt();
+                        mccain = mccain + elect.nextInt(); //add to mccain sum
                     }
                     else {
-                        String booboo = elect.next();
+                        String booboo = elect.next(); //consume
                     }
-                    count++;
+                    count++; //count up
                     }
-                String texty = elect.next();
+                String texty = elect.next(); //consume
                 }
             }
         System.out.println("Obama: " + obama);
